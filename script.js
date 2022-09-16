09325b043fc5f7a9ee25857906ecb33e598c67b1
Francesco Paletta <francesco.paletta96@libero.it>
2022-09-16 15:30:37 +0200

    Prova non funzionante con il for

--- script.js
+++ script.js
@@ -3,16 +3,16 @@ let numfacce = 8
 
 document.write("Hai scelto di usare il dado con " + numfacce + " facce" + br);
 
-let num = Math.round ( Math.random() * numfacce);
-document.write("Il numero uscito è: " + num);
 
-while (num ==0 ){
-    let num = Math.round ( Math.random() * numfacce);
-    document.write("Il numero uscito è: " + num);
-}
+let num = Math.round ( Math.random() * numfacce);
+document.write("Il numero uscito è: " + num + br);
 
 for (i=0; i<100; i++) {
     let num = Math.round ( Math.random() * numfacce);
-    document.write("Il numero uscito è: " + num);
-}
+    document.write("Il numero uscito è: " + num + br);
+
+    while (num ==0 ){
+        let num = Math.round ( Math.random() * numfacce);
+        document.write("Il numero uscito è: " + num);
+    }
 }
