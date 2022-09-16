66402895951401c1bec0e683ceadc50889493af9
Francesco Paletta <francesco.paletta96@libero.it>
2022-09-16 16:04:07 +0200

    prova non funzionante con if dove devo far ripetere in caso di numero negativo

--- script.js
+++ script.js
@@ -5,7 +5,16 @@ document.write("Hai scelto di usare il dado con " + numfacce + " facce" + br);
 
 let num = Math.round ( Math.random() * numfacce);
 
+
+if ( numfacce >0 ) {
 while (num ==0 ){
     num = Math.round ( Math.random() * numfacce);
 }
 document.write("Il numero uscito è: " + num);
+} else {
+    window.alert("Ho detto un numero positivo")
+    let numfacce = prompt("Di quante facce è il dado? (inserisci un numero intero)");
+
+document.write("Hai scelto di usare il dado con " + numfacce + " facce" + br);
+}
+
