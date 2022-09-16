31d28aa299035fa4df7b32b1ae85af95b442424a
Francesco Paletta <francesco.paletta96@libero.it>
2022-09-16 15:50:48 +0200

    Prova dado funziona con while per lo zero

--- script.js
+++ script.js
@@ -4,9 +4,9 @@ let numfacce = prompt("Di quante facce è il dado? (inserisci un numero intero)"
 document.write("Hai scelto di usare il dado con " + numfacce + " facce" + br);
 
 let num = Math.round ( Math.random() * numfacce);
-document.write("Il numero uscito è: " + num);
+
 
 while (num ==0 ){
-    let num = Math.round ( Math.random() * numfacce);
-    document.write("Il numero uscito è: " + num);
+    num = Math.round ( Math.random() * numfacce);
 }
+document.write("Il numero uscito è: " + num);
